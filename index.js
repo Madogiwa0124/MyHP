086dc8affb0bf42ecf24b99c025bc6d7fc27e7d1
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2017-09-25 00:02:33 +0900

    ブログ関連の記述を削除

--- index.js
+++ index.js
@@ -75,22 +75,4 @@ window.onload = function () {
   apps.forEach(function (appInfo) {
     document.getElementById("myapps").appendChild(createAppHtml(appInfo));
   });
-
-  // BlogのHTMLを取得
-  // $.ajax({
-  //   url: 'http://madogiwa0124.hatenablog.com/',
-  //   dataType: 'html'
-  // })
-  // .done(function (data) {
-  //   // 最新の記事の部分を抽出
-  //   var blogs = $(data).find('.recent-entries');
-  //   // 抽出した要素の整形
-  //   var index = 1;
-  //   blogs.find('.recent-entries-item').each(function () {
-  //     createBlogHtml(this, index);
-  //     index++;
-  //   });
-  //   // HPに埋め込み
-  //   $('#blogs').append(blogs);
-  // });
 }
